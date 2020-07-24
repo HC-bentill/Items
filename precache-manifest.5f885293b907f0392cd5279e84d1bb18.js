fcbc87bfbe61bc5a25c89e71fc4d6c2a354a9845
HC-bentill <henrybentil88@gmail.com>
2020-07-24 02:47:12 +0000

    Updates

---
+++ precache-manifest.5f885293b907f0392cd5279e84d1bb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f8c8c285413546300d63a31977952c",
+    "url": "/Items/index.html"
+  },
+  {
+    "revision": "a2b880a9e76d2319de29",
+    "url": "/Items/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3113bf9566074543e20",
+    "url": "/Items/static/js/2.1076a735.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Items/static/js/2.1076a735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b880a9e76d2319de29",
+    "url": "/Items/static/js/main.9fbf64f9.chunk.js"
+  },
+  {
+    "revision": "4fc1907af9bcd925dc2f",
+    "url": "/Items/static/js/runtime-main.d8032323.js"
+  }
+]);
